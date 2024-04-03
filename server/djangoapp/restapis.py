f2109fdc667afa719e617ea364c1b186392f4105
Sebastian Robi <76728638+seb-rob@users.noreply.github.com>
2024-04-03 14:18:30 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,19 +3,20 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
+    'sentiment_analyzer_url', 
     default="http://localhost:5050/")
 
-def get_request(endpoint, **kwargs):
-    params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+def get_request(endpoint, **kwargs): 
+    params = "" 
+    if(kwargs): 
+        for key, value in kwargs.items(): 
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -24,7 +25,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err: 
         # If any error occurs
         print("Network exception occurred")
 
@@ -34,7 +35,7 @@ def analyze_review_sentiments(text):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
+    except Exception as err: 
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
